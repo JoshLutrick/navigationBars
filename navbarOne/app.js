90e7e3e571263b6a85e67909358659f0dbe198e7
JoshLutrick <joshslutrick@gmail.com>
2024-06-11 13:10:33 -0700

    Added full functionality to the burger menu

--- navbarOne/app.js
+++ navbarOne/app.js
@@ -11,8 +11,12 @@ const navObj = {
 function toggleNav(){
     if(navObj.navMenu.classList.contains('showNav')){
         navObj.navMenu.classList.toggle('showNav')
+        navObj.burgerIcon.classList.add('fa-bars')
+        navObj.burgerIcon.classList.remove('fa-x')
     } else if (!navObj.navMenu.classList.contains('showNav')){
         navObj.navMenu.classList.toggle('showNav')
+        navObj.burgerIcon.classList.remove('fa-bars')
+        navObj.burgerIcon.classList.add('fa-x')
     }
 }
 
